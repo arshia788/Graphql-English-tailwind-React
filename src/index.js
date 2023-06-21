eeacb758c12435ecd86340898d7b614b39b13db9
Amirali Amanipour <arshia.ala80@gmail.com>
2023-06-22 01:14:10 +0330

    added the env and the components

--- src/index.js
+++ src/index.js
@@ -1,17 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+import './index.css';
+
+import {ApolloClient, ApolloProvider, InMemoryCache} from "@apollo/client"
+
+const client= new ApolloClient({
+  uri:'https://api-us-west-2.hygraph.com/v2/clj5eru8202m801unfmluby99/master',
+  cache:new InMemoryCache()
+})
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
+  <ApolloProvider client={client}>
     <App />
-  </React.StrictMode>
+  </ApolloProvider>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
