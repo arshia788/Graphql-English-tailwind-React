ca3aed06edb5949ac1e42ee20fa052dc8324478e
Amirali Amanipour <arshia.ala80@gmail.com>
2023-06-22 01:24:11 +0330

    added the fonts

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,12 +1,17 @@
 import React from 'react';
 
+import { FaHome } from "react-icons/fa";
+
 const Header = () => {
-    
 
     return (
-        <div>
+        <nav className='bg-blue-700 flex justify-between items-center py-2 px-20 text-white'>
+            <h1 className='text-3xl'>weblog</h1>
+            <FaHome 
+            className='text-2xl'
+            />
             
-        </div>
+        </nav>
     );
 };
 
